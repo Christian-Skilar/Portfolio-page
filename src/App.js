9c10a62c4e8305c29f4378d25567718c34713ae6
Christian-Skilar <57668355+Christian-Skilar@users.noreply.github.com>
2022-12-22 19:06:14 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import ScaleLoader from "react-spinners/ScaleLoader";
 import { About, Footer, Header, Skills } from './container';
 import { Navbar } from './components';
 import './App.scss';
 
 const App = () => {
 
-  return (
-        <div className="app">
-        <Navbar />
-        <Header />
-        <About />
-        <Skills />
-        <Footer />
-      </div>
-    );
-  }
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+return (
+  <div className="app">
+    {loading ? (
+        <div className='loading'>
+          <ScaleLoader color="#2AB0EC" width="10px" height="70px" />
+        </div>
+      ) : (
+        <div>
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Footer />
+        </div>
+      )}
+  </div>
+);
+}
 
 export default App;
